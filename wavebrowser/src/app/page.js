44319787e3502022faaee02b2be6ddaa9311fd8b
Lyndon <lyndon@onthelink.net>
2024-12-24 00:00:22 +1300

    Start of the webapp

---
+++ wavebrowser/src/app/page.js
@@ -0,0 +1,30 @@
+export default function Home() {
+  return (
+	<main>
+
+
+
+
+      <div className="text-center mt-4 col-md-6 mx-auto"> 
+        <h1>WaveBrowser</h1>
+ 
+<div className="card mb-3" style={{ maxWidth: "540px" }}>
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src="..." className="img-fluid rounded-start" alt="..." />
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+      
+      </div>
+	</main>
+  )
+}
